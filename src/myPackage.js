9e63edad09b56da1edc70641cb6eb9b5a4e0d0d1
Hanbyoul <dytjqdla120@naver.com>
2023-01-23 00:17:33 +0900

    Js-Doc Practiec

--- src/myPackage.js
+++ src/myPackage.js
@@ -1,7 +1,22 @@
+//JS-Doc을 이용한 javascript 내부에서 직접 type 설정.
+
+//@ts-check
+/**
+ * Initialize the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url
+ * @returns boolean
+ */
 export function init(config) {
-  return true;
+  return console.log("hello");
 }
 
+/**
+ * Exits the program
+ * @param {number} code
+ * @returns number
+ */
 export function exit(code) {
   return code + 1;
 }

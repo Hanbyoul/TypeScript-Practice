8edf16a816b10a8abd0b56d03e82bc9f314d556d
Hanbyoul <dytjqdla120@naver.com>
2023-01-24 21:41:47 +0900

    create block chain

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,54 @@
-import { init, exit } from "./myPackage";
+import crypto from "crypto";
 
-init({
-  debug: true,
-  url: "hello",
-});
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
 
-exit(4);
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
+  }
+}
+class Blockchain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    // return this.blocks // Bad  다이렉트로 block 에 연결하여 return 하는 보안의 위험성.
+    return [...this.blocks]; // good 새로운 배열을 만들어 block을 추가.
+  }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+
+console.log(blockchain.getBlocks());
